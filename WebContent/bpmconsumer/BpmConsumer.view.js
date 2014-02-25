ab465cf8b1b89c1fcbe659cea9d095ae9dd8c2bc
Jeetendra <choudhary.jeetendra@gmail.com>
2014-02-25 22:12:16 +0800

    BpmConsumerApp
    Bpm Odata service consumer app

---
+++ WebContent/bpmconsumer/BpmConsumer.view.js
@@ -0,0 +1,19 @@
+sap.ui.jsview("bpmconsumer.BpmConsumer", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf bpmconsumer.BpmConsumer
+	*/ 
+	getControllerName : function() {
+		return "bpmconsumer.BpmConsumer";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf bpmconsumer.BpmConsumer
+	*/ 
+	createContent : function(oController) {
+
+	}
+
+});
